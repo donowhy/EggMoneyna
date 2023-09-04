b5a5b119e1d19ff5db1fd7da7da9fc38fd056acc
eunha812 <0812keh@naver.com>
2023-09-04 22:44:31 +0900

    ✨ feat : [첫화면 설정] | [김은하] | [MainActivity.kt]

--- android/app/src/main/java/com/shbhack/eggmoneyna/MainActivity.kt
+++ android/app/src/main/java/com/shbhack/eggmoneyna/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.shbhack.eggmoneyna.ui.EggMoneynaApp
 import com.shbhack.eggmoneyna.ui.theme.EggMoneynaTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EggMoneynaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    EggMoneynaApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EggMoneynaTheme {
-        Greeting("Android")
     }
 }
