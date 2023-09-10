e2d6065fbc63f78ef2cb00912fefeeeb25f15171
김진영 <kimjinzer099@gmail.com>
2023-09-10 23:55:10 +0900

    ✏ modify : [api 버전 어노테이션 추가] | [김진영] | [EggMoneynaNavGraph.kt]

--- android/app/src/main/java/com/shbhack/eggmoneyna/ui/EggMoneynaApp.kt
+++ android/app/src/main/java/com/shbhack/eggmoneyna/ui/EggMoneynaApp.kt
@@ -1,5 +1,7 @@
 package com.shbhack.eggmoneyna.ui
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -7,10 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.shbhack.eggmoneyna.ui.theme.EggMoneynaTheme
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun EggMoneynaApp(
 ) {
