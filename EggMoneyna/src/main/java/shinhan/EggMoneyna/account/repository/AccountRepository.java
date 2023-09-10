e71c5ec9dae95218a7b17f6f3f6883a6a444d125
강민승 <>
2023-09-11 00:07:35 +0900

    🛠 fix : [fix 계좌 개성 로직 수정 및 account Repo 수정] | [강민승] | [account]

--- EggMoneyna/src/main/java/shinhan/EggMoneyna/account/repository/AccountRepository.java
+++ EggMoneyna/src/main/java/shinhan/EggMoneyna/account/repository/AccountRepository.java
@@ -3,5 +3,9 @@ package shinhan.EggMoneyna.account.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shinhan.EggMoneyna.account.entity.Account;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccountNumber(Long accountNumber);
 }
