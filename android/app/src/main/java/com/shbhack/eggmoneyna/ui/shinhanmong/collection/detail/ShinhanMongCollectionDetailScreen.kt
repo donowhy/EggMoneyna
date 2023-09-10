60a84fac66b7e3bc91f58713d9174f9899e472d1
김진영 <kimjinzer099@gmail.com>
2023-09-10 23:59:57 +0900

    ✨ feat : [신한몽 도감 세부 페이지 파일 생성] | [김진영] | [ShinhanMongDetailCollectionScreen.kt] #14

---
+++ android/app/src/main/java/com/shbhack/eggmoneyna/ui/shinhanmong/collection/detail/ShinhanMongCollectionDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.shbhack.eggmoneyna.ui.shinhanmong.collection.detail
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ShinhanMongCollectionDetailScreen(navController: NavController) {
+
+}
