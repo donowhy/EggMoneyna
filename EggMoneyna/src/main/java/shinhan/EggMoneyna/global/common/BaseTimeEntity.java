53af0064c85c2f70ff3518e0f98e48b07abdc529
강민승 <>
2023-09-11 00:05:18 +0900

    ✨ feat : [firebase] | [강민승] | [firebase]

---
+++ EggMoneyna/src/main/java/shinhan/EggMoneyna/global/common/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package shinhan.EggMoneyna.global.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass // 자식에 매핑(db에 반영x)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+}
