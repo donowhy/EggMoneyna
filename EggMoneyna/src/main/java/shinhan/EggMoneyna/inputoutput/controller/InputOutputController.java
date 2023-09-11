49b11ce1338583b3a705f9d2dcaa8d20a14717e8
Giggle1998 <rlvy98@naver.com>
2023-09-11 22:57:52 +0900

    ✏ modify : [URI 수정] | [이기표] | [InputOutputController.java]

--- EggMoneyna/src/main/java/shinhan/EggMoneyna/inputoutput/controller/InputOutputController.java
+++ EggMoneyna/src/main/java/shinhan/EggMoneyna/inputoutput/controller/InputOutputController.java
@@ -18,25 +18,25 @@ public class InputOutputController {
     private final InputOutputService inputOutputService;
 
     @Operation(summary = "입금 추가", description = "사용자 계좌에 입금이 추가됩니다.", tags = { "InputOutput Controller" })
-    @PostMapping("/input")
+    @PostMapping("/in")
     public ResponseEntity<AddInputOutputResponseDto> addInput(@UserInfo UsersInfo usersInfo, @RequestBody AddInputOutRequestDto addInputOutRequestDto) {
         return ResponseEntity.ok(inputOutputService.addInput(usersInfo.getId(), addInputOutRequestDto));
     }
 
     @Operation(summary = "출금 추가", description = "사용자 계좌에 출금이 추가됩니다.", tags = { "InputOutput Controller" })
-    @PostMapping("/output")
+    @PostMapping("/out")
     public ResponseEntity<AddInputOutputResponseDto> addOutput(@UserInfo UsersInfo usersInfo, @RequestBody AddInputOutRequestDto addInputOutRequestDto) {
         return ResponseEntity.ok(inputOutputService.addOutput(usersInfo.getId(), addInputOutRequestDto));
     }
 
     @Operation(summary = "당일 입금 조회", description = "사용자 계좌에 해당하는 날짜의 입금내역을 조회합니다.", tags = { "InputOutput Controller" })
-    @PostMapping("/input/{inputDate}")
+    @PostMapping("/in/{inputDate}")
     public ResponseEntity<InputOutputResponseDto> getInput(@UserInfo UsersInfo usersInfo, @PathVariable String inputDate) {
         return ResponseEntity.ok(inputOutputService.getInput(usersInfo.getId(), inputDate));
     }
 
     @Operation(summary = "당일 출금 조회", description = "사용자 계좌에 해당하는 날짜의 출금내역을 조회합니다.", tags = { "InputOutput Controller" })
-    @PostMapping("/output/{outputDate}")
+    @PostMapping("/out/{outputDate}")
     public ResponseEntity<InputOutputResponseDto> getOutput(@UserInfo UsersInfo usersInfo, @PathVariable String outputDate) {
         return ResponseEntity.ok(inputOutputService.getOutput(usersInfo.getId(), outputDate));
     }
