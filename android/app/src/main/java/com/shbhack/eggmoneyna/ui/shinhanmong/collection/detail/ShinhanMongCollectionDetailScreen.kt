62995aee02f000902904c09d6a8f78113727ea19
김진영 <kimjinzer099@gmail.com>
2023-09-11 22:52:54 +0900

    ✨ feat : [신한몽 도감 세부 페이지 구현 완료] | [김진영] | [ShinhanMongCollelctionDetailScreen.kt] #14

--- android/app/src/main/java/com/shbhack/eggmoneyna/ui/shinhanmong/collection/detail/ShinhanMongCollectionDetailScreen.kt
+++ android/app/src/main/java/com/shbhack/eggmoneyna/ui/shinhanmong/collection/detail/ShinhanMongCollectionDetailScreen.kt
@@ -1,9 +1,27 @@
 package com.shbhack.eggmoneyna.ui.shinhanmong.collection.detail
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.shbhack.eggmoneyna.R
+import com.shbhack.eggmoneyna.ui.common.top.TopWithBack
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ShinhanMongCollectionDetailScreen(navController: NavController) {
 
+    Scaffold(
+        topBar = {
+            TopWithBack(
+                navController = navController,
+                title = stringResource(id = R.string.shinhanmong_collection_appbar_title)
+            )
+        }
+    ) { paddingValues ->
+        ShinhanMongCollectionDetailView(paddingValues)
+    }
 }
+
