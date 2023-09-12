6e18a11ab729fffeb7bd699878cae85041016a5d
eunha812 <0812keh@naver.com>
2023-09-12 21:48:05 +0900

    ✨ feat : [system bar padding 추가 및 제거 함수] | [김은하] | [MainActivity.kt]

--- android/app/src/main/java/com/shbhack/eggmoneyna/MainActivity.kt
+++ android/app/src/main/java/com/shbhack/eggmoneyna/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.shbhack.eggmoneyna
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.shbhack.eggmoneyna.ui.EggMoneynaApp
 import com.shbhack.eggmoneyna.ui.theme.EggMoneynaTheme
 
@@ -15,11 +18,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             EggMoneynaTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
-                    EggMoneynaApp()
+                    EggMoneynaApp(this)
                 }
             }
+        }
+        setSystemBarTransparent()
+    }
+
+    fun setSystemBarTransparent() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        if (Build.VERSION.SDK_INT >= 30) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+
+    fun setStatusBarOrigin() {
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
+        if (Build.VERSION.SDK_INT >= 30) {    // API 30 에 적용
+            WindowCompat.setDecorFitsSystemWindows(window, true)
         }
     }
 }
