f6fe81050c16c4f0493f6000d9f53bee48655938
eunha812 <0812keh@naver.com>
2023-09-12 21:46:51 +0900

    🏗 chore : [pager 라이브러리 추가] | [김은하] | [app/build.gradle.kts]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -107,6 +107,10 @@ dependencies {
 
     // datetime
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
+
+    // Pager
+    implementation ("com.google.accompanist:accompanist-pager:0.20.1")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.20.1")
 }
 
 kapt {
