d051b825665f64d5af41f03c5fc3ce5fa468d83d
강민승 <k4minseung@gmail.com>
2023-09-14 02:51:45 +0900

    🛠 fix : [swagger 에 jwt 토큰기능 넣어두기] | [강민승] | [SwaggerConfig]

--- EggMoneyna/src/main/java/shinhan/EggMoneyna/global/config/SwaggerConfig.java
+++ EggMoneyna/src/main/java/shinhan/EggMoneyna/global/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package shinhan.EggMoneyna.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,13 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(title = "에그머니나 API 명세서",
                 description = "게이미피케이션과 케어를 통한 아이의 소비습관을 책임질 에그머니나!",
                 version = "v1"))
+
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
