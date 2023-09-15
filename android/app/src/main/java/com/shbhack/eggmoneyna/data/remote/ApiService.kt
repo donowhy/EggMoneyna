cfb0cc06ae0fe55c59abc01c0f841f81209346bd
김진영 <kimjinzer099@gmail.com>
2023-09-15 14:01:02 +0900

    ✨ feat : [신한몽 생성, 조회 api 통신 코드 작성] | [김진영] | [ApiService.kt]

--- android/app/src/main/java/com/shbhack/eggmoneyna/data/remote/ApiService.kt
+++ android/app/src/main/java/com/shbhack/eggmoneyna/data/remote/ApiService.kt
@@ -1,4 +1,17 @@
 package com.shbhack.eggmoneyna.data.remote
 
+import com.shbhack.eggmoneyna.data.model.ErrorResponse
+import com.shbhack.eggmoneyna.data.model.MonsterResponseDto
+import com.shbhack.eggmoneyna.data.model.MonsterSaveResponseDto
+import com.shbhack.eggmoneyna.util.network.NetworkResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ApiService {
+    // 신한몽
+    @POST("/monsters/save")
+    suspend fun save(): NetworkResponse<MonsterSaveResponseDto, ErrorResponse>
+
+    @GET("/monsters/getMyMong")
+    suspend fun getMyMong(): NetworkResponse<MonsterResponseDto, ErrorResponse>
 }
--- android/app/src/main/java/com/shbhack/eggmoneyna/data/repository/main/MainRepository.kt
+++ android/app/src/main/java/com/shbhack/eggmoneyna/data/repository/main/MainRepository.kt
@@ -1,4 +1,15 @@
 package com.shbhack.eggmoneyna.data.repository.main
 
+import com.shbhack.eggmoneyna.data.model.ErrorResponse
+import com.shbhack.eggmoneyna.data.model.MonsterResponseDto
+import com.shbhack.eggmoneyna.data.model.MonsterSaveResponseDto
+import com.shbhack.eggmoneyna.util.network.NetworkResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface MainRepository {
+    // 신한몽
+    suspend fun save(): NetworkResponse<MonsterSaveResponseDto, ErrorResponse>
+
+    suspend fun getMyMong(): NetworkResponse<MonsterResponseDto, ErrorResponse>
 }
