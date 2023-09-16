6953ee52195cd2e5e8b062101c1ef8d97aac761a
강민승 <k4minseung@gmail.com>
2023-09-16 09:16:50 +0900

    🛠 fix : [swagger adjust] | [강민승] | [controller]

--- EggMoneyna/src/main/java/shinhan/EggMoneyna/wishbox/controller/WishboxController.java
+++ EggMoneyna/src/main/java/shinhan/EggMoneyna/wishbox/controller/WishboxController.java
@@ -1,6 +1,7 @@
 package shinhan.EggMoneyna.wishbox.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,7 @@ public class WishboxController {
 
     private final WishboxService wishboxService;
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @Operation(summary = "위시박스 생성", description = "위시박스 생성 시 가상계좌 생성", tags = { "Wishbox Controller" })
     @PostMapping("/create")
     public String create(@UserInfo UsersInfo usersInfo, @RequestBody CreateWishBoxRequestDto requestDto){
@@ -30,24 +32,29 @@ public class WishboxController {
         return "생성 성공";
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @Operation(summary = "위시박스 별명 변경", description = "위시박스 별명 변경", tags = { "Wishbox Controller" })
     @PutMapping("/nickname")
     public void updateNickname(@UserInfo UsersInfo usersInfo, @RequestBody UpdateNicknameRequest request){
         wishboxService.updateNickname(usersInfo.getId(), request);
     }
 
+
+    @SecurityRequirement(name = "Bearer Authentication")
     @Operation(summary = "위시박스 삭제", description = "위시박스 삭제, 삭제하면 돈은 모계좌로 돌아간다.", tags = { "Wishbox Controller" })
     @DeleteMapping("/delete")
     public void deleteWishbox(@UserInfo UsersInfo usersInfo, @RequestBody GetWishboxRequest request){
         wishboxService.deleteWishbox(usersInfo.getId(), request);
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @Operation(summary = "위시박스 단일 조회", description = "위시박스 단일 조회", tags = { "Wishbox Controller" })
     @GetMapping("/getOneWishbox")
     public GetWishboxResponse getOneWishbox (@UserInfo UsersInfo usersInfo, @RequestBody GetWishboxRequest request){
         return wishboxService.getOneWishbox(usersInfo.getId(), request);
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @Operation(summary = "위시박스 전체 조회", description = "위시박스 전체 조회", tags = { "Wishbox Controller" })
     @GetMapping("/getWishbox")
     public List<GetWishboxResponse> getWishbox (@UserInfo UsersInfo usersInfo){
