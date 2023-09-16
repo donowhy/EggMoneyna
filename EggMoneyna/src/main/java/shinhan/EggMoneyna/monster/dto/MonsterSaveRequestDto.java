316f3b491bdc26e19839eb7de1d63460dc545ead
강민승 <k4minseung@gmail.com>
2023-09-16 11:06:42 +0900

    🛠 fix : [신한몽 생성 수정] | [강민승] | [MonsterSaveRequestDto, MonsterSaveResponseDto, MonsterService]

--- EggMoneyna/src/main/java/shinhan/EggMoneyna/monster/dto/MonsterSaveRequestDto.java
+++ EggMoneyna/src/main/java/shinhan/EggMoneyna/monster/dto/MonsterSaveRequestDto.java
@@ -2,22 +2,16 @@ package shinhan.EggMoneyna.monster.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import shinhan.EggMoneyna.monster.entity.enumType.Benefit;
 
 @Getter
+@NoArgsConstructor
 public class MonsterSaveRequestDto {
-    private String benefit;
+    private Benefit benefit;
 
     @Builder
-    public MonsterSaveRequestDto(String benefit) {
+    public MonsterSaveRequestDto(Benefit benefit) {
         this.benefit = benefit;
-    }
-
-    public Benefit getBenefitEnum() {
-        try {
-            return Benefit.valueOf(benefit);
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Invalid benefit value: " + benefit);
-        }
     }
 }
